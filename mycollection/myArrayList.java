be34f5a666f9a5f21428fbcacbd81c66f9d88df0
Tunay Novruz <tunaynovruz@gmail.com>
2019-08-14 10:28:49 +0400

    first commit

---
+++ mycollection/myArrayList.java
@@ -0,0 +1,23 @@
+package mycollection;
+
+import java.util.ArrayList;
+
+class myArrayList {
+    public static void main(String[] args) {
+        ArrayList <String> list1 = new ArrayList<>();
+        list1.add("salam");
+        list1.add("dunya");
+        list1.set(1,"zzzz");
+        list1.remove(0);
+        System.out.println(list1); 
+
+        ArrayList<String>list2 = new ArrayList<>();
+
+        list2.add("ikinciden");
+
+        list1.addAll(list2);
+
+        System.out.println(list1);
+
+    }
+}
