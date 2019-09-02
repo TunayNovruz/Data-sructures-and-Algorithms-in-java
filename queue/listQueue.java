81d10c815ec7349acc8942be2f533e42406219f5
Tunay Novruz <tunaynovruz@gmail.com>
2019-09-03 00:10:15 +0400

    queue step-1

---
+++ queue/listQueue.java
@@ -0,0 +1,40 @@
+package queue;
+
+class ListQueue <E> {
+    Node <E> first;
+    Node <E> last; 
+    int size;
+    int maxSize = Integer.MAX_VALUE;
+
+    class Node <T> {
+        Node <T> next;
+        T data;
+        Node(T value){
+            data = value;
+        }
+    }
+
+    void enqueue(E data){
+        Node <E> newNode = new Node <E>(data);
+        if(first == null){
+            first = newNode;
+            last = first;        
+        }else{
+            last.next = newNode;
+            last = newNode;
+        }
+        size++;
+    }
+
+    E dequeue(){
+        if(first == null){
+            return null;
+        }else{
+            Node <E> temp = first;
+            first = first.next;
+            size--;
+            return temp.data;
+        }
+    }
+
+}
