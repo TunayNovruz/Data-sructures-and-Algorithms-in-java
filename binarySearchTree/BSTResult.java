de136ab5b8a94d46b34dfee42f0d79349c8d7f2a
Tunay Novruz <tunaynovruz@gmail.com>
2019-09-08 00:37:49 +0400

    BST step-2  (find,delete,display methods added)

--- binarySearchTree/BSTResult.java
+++ binarySearchTree/BSTResult.java
@@ -5,13 +5,26 @@ class BSTResult {
     public static void main(String args[]){
         BinarySearchTree <Integer> myTree = new BinarySearchTree<Integer>();
         
-        myTree.insert(5);
+        myTree.insert(10);
+        myTree.insert(8);        
+        myTree.insert(7);
+        myTree.insert(9);
         myTree.insert(6);
-        myTree.insert(4);
-        myTree.insert(8);
+        myTree.insert(13);
+        myTree.insert(11);
+        myTree.insert(12);
+        myTree.insert(14);
+
+        //myTree.deleteByValue(14);
         
-        System.err.println(myTree.root.data);
-        System.err.println(myTree.root.left.data);
-        System.err.println(myTree.root.right.right.data);
+        System.err.println(myTree.root.right.data);
+
+        BSTNode <Integer> found = myTree.findByValue(11);
+
+        System.out.println("tapilan: "+found.data);
+
+        myTree.display();
+
+        
     }
 }
--- binarySearchTree/binarySearchTree.java
+++ binarySearchTree/binarySearchTree.java
@@ -34,6 +34,8 @@ class BinarySearchTree <T extends Integer> {
         
         //addChild(root,newChild);
     }
+
+    // rekursiv üsul tam deyil,gələcəkdə birdə baxacam
     void addChild(BSTNode<T>node, BSTNode<T>child){
         if(node == null){
             node = child;
@@ -46,4 +48,89 @@ class BinarySearchTree <T extends Integer> {
         }
     }
 
+    BSTNode<T> findByValue(T value){    
+        return findFromNode(root,value);
+    }
+
+    BSTNode<T> findFromNode(BSTNode<T> node , T value){
+
+        BSTNode<T> result = new BSTNode<T>(null);
+        if(node != null){
+            if(node.data == value){
+                result = node;
+            }
+            else if(node.data <= value && node.left != null){
+                result = findFromNode(node.left,value);
+            }
+            else if(node.right != null){
+                result =  findFromNode(node.right, value);
+            }
+        }
+        return result;
+       
+    }
+
+    void deleteByValue(T value){
+       root =  deleteFromNode(root,value);
+    }
+
+    BSTNode<T> deleteFromNode(BSTNode<T>node,T value){
+        //əgər node nulldursa deməli treedə bu qiymət yoxdu
+        if(node == null){
+            return root;
+        }
+
+        if( value < node.data){
+            node.left = deleteFromNode(node.left,value);
+        }else if(value > node.data){
+            node.right = deleteFromNode(node.right, value);
+        }else{
+            //demeli sileceyimiz nodu tapdiq
+            
+            //eger hec bir childi yoxdursa
+            if(node.right == null && node.left == null){
+                node = null;
+            }
+            //eger iki childi varsa 
+            else if(node.right != null && node.left != null){
+                //indi bu nodu evezleyeceyimiz sagdaki en kicik nodu tapaq
+                T minimum =  findMinimum(node.right);
+                node.data = minimum;
+                //artiq datamizi sildik amma minimumdan iki eded var demeli onu silmek lazimdi
+                deleteFromNode(node.right,minimum);
+            }
+            //herhansi bir node nulldursa
+            else {
+                if(node.right == null){
+                    node = node.left;
+                }else{
+                    node = node.right;
+                }
+            }        
+        }
+        return node;
+    }
+
+    T findMinimum(BSTNode <T> node){
+        T min = node.data;
+        BSTNode <T>  temp = node;
+        while(temp.left != null){
+            min = temp.left.data;
+            temp = temp.left;            
+        }
+
+        return min;
+    }
+
+    void display(){
+        displayFromNode(root);
+    }
+    void displayFromNode(BSTNode<T> node){
+        if(node == null){
+            return;
+        }
+        System.out.print("-"+node.data);
+        displayFromNode(node.left);    
+        displayFromNode(node.right);
+    }
 }
