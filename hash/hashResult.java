b9f071cd0ca6b7aa8f122f475cfe2685b9f3266f
tunaynovruz <tunaynovruz@gmail.com>
2019-09-22 22:58:40 +0400

    unordered hashmap implemented

---
+++ hash/hashResult.java
@@ -0,0 +1,18 @@
+package hash;
+import hash.HashMap;
+
+class HashResult {
+    public static void main(String[] args) {
+        HashMap<String,String> myMap = new HashMap <String,String>(15);
+
+        myMap.insert("first","birinci");
+        myMap.insert("second","ikinic");
+        myMap.insert("third","ucuncu");
+        myMap.insert("forth","dorduncu");
+
+        myMap.displayMap();
+
+
+        //System.out.println("hello world".hashCode());
+    }
+}
