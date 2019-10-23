6f36cd1ecc021d15338915367ea91c84899d954e
tunaynovruz <tunaynovruz@gmail.com>
2019-10-24 00:07:08 +0400

    Selection sort

--- src/com/algorithms/SelectionSort.java
+++ src/com/algorithms/SelectionSort.java
@@ -1,4 +1,28 @@
 package com.algorithms;
 
 public class SelectionSort {
+    public static void main(String[] args) {
+        int[] nums = {8, 4, 0, 2, 9, 5, 6};
+        int[] sortedNums = sort(nums);
+        for (int sortedNum : sortedNums) {
+            System.out.println(sortedNum);
+        }
+
+    }
+
+    private static int[] sort(int[] arr) {
+        int count = arr.length;
+        int k = 0;
+        for (int i = 0; i < count - 1; i++) {
+            int min_index = i;
+            for (int j = i + 1; j < count; j++)
+                if (arr[j] < arr[min_index]) min_index = j;
+
+            int temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+        }
+        return arr;
+    }
+
 }
