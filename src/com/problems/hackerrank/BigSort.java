3c00878ce397f95bfaa7a9bd99ef7abacb7eb346
tunaynovruz <tunaynovruz@gmail.com>
2019-11-06 00:16:07 +0400

    [ Hackerrank - bigsort (time limit error) ]

---
+++ src/com/problems/hackerrank/BigSort.java
@@ -0,0 +1,43 @@
+package com.problems.hackerrank;
+
+public class BigSort {
+
+    public static void main(String[] args) {
+        String[] nums = {"23", "322222222223333", "23234234", "5"};
+        String[] sorted = bigSorting(nums);
+        for (String s : sorted)
+            System.out.print(s + ",");
+
+
+    }
+
+    private static String[] bigSorting(String[] unsorted) {
+        int len = unsorted.length;
+        for (int i = 0; i < len - 1; i++) {
+            int min_index = i;
+            for (int j = i + 1; j < len; j++) {
+                if (unsorted[j].length() < unsorted[min_index].length())
+                    min_index = j;
+                else if (unsorted[j].length() == unsorted[min_index].length()) {
+                    int chLen = unsorted[j].length();
+                    for (int k = 0; k < chLen; k++) {
+                        if (unsorted[j].charAt(k) < unsorted[min_index].charAt(k)) {
+                            min_index = j;
+                            break;
+                        } else if (unsorted[j].charAt(k) > unsorted[min_index].charAt(k)) {
+                            break;
+                        }
+                    }
+                }
+            }
+            swap(unsorted, i, min_index);
+        }
+        return unsorted;
+    }
+
+    private static void swap(String[] arr, int i, int j) {
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
